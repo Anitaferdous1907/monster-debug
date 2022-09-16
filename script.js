e42e33c96c774babf76dd029728a02bac052687d
Anitaferdous1907 <anitaferdous009@gmail.com>
2022-09-17 03:12:46 +0600

    button fix

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 const display = document.getElementById("display");
 const question = document.getElementById("question");
 const startBtn = document.getElementById("starts");
-
-
-
 const countdownOverlay = document.getElementById("countdown");
 const resultModal = document.getElementById("result");
 const modalBackground = document.getElementById("modal-background");
@@ -134,7 +131,7 @@ const start = () => {
 };
 
 // START Countdown
-startBtn.addEventListener("click", start)
+document.getElementById("starts").addEventListener('click', start);
 // If history exists, show it
 displayHistory();
 
